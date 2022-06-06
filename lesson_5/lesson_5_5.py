a8faa04e45fb8883186a60871fb5847b9d6dfc61
sklleon <sklleon@mail.ru>
2022-06-06 17:04:38 +0300

    homework lesson 5

---
+++ lesson_5/lesson_5_5.py
@@ -0,0 +1,19 @@
+# Создать (программно) текстовый файл, записать в него программно набор чисел, разделённых пробелами.
+# Программа должна подсчитывать сумму чисел в файле и выводить её на экран.
+
+
+def summary():
+    try:
+        with open('file_5.txt', 'w+') as file_obj:
+            line = input('Введите цифры через пробел \n')
+            file_obj.writelines(line)
+            my_numb = line.split()
+
+            print(sum(map(int, my_numb)))
+    except IOError:
+        print(f'Ошибка в файле {IOError}!')
+    except ValueError:
+        print(f"Произошла ошибка {IOError} ввода-вывода!")
+
+
+summary()
